1f5d7620f6d67e12594d390cff251ba55755de97
Michelle Wong <michellekwong2@gmail.com>
2019-09-24 21:24:16 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -22,7 +22,7 @@ def monopoly_with_third_tier
   # they'll guide you in how to modify what you just did to have more
   # complexity.
   base_hash = {
-	  :railroads => {pieces: 4, rent_in_dollars: {names:"one_piece_owned", names_two: "two_piece_owned", names_three: "three_piece_owned", names_four: "four_piece_owned"}, names: "names"}
+	  :railroads => {pieces: 4, rent_in_dollars: {rent_key_one:"one_piece_owned", rent_key_two: "two_piece_owned", rent_key_three: "three_piece_owned", rent_key_four: "four_piece_owned"}, names: "names"}
 	}
 end
 
